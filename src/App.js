2fd739f606387192981771477acb0e929b1ba559
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2019-12-11 10:49:42 -0500

    finished MVP

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import TopRow from "./TopRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
+
+  const changeScore = (teamName, amount) => {
+    teamName === 'home'? setHomeScore(homeScore + amount) : setAwayScore(awayScore + amount);
+  }
+
+  const changeQuarter = () =>{
+    setQuarter(quarter % 4 + 1)
+  }
 
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">32</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
-          </div>
-        </div>
-        <BottomRow />
+        <TopRow homeScore={homeScore} awayScore={awayScore}/>
+        <BottomRow quarter={quarter}/>
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
+      <ButtonGroup changeScore={changeScore} changeQuarter={changeQuarter}/>
     </div>
   );
 }
 
+function ButtonGroup({changeScore, changeQuarter}){
+  return(
+    <section className="buttons">
+        <div className="homeButtons">
+          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          <button onClick={() => changeScore('home',7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={() => changeScore('home',3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+        </div>
+        <div className="awayButtons">
+          <button onClick={() => changeScore('away',7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => changeScore('away',3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+        </div>
+        <button onClick={() => changeQuarter()} className="awayButtons__fieldGoal">Change Quarter</button>
+      </section> 
+  )
+}
+
 export default App;
+
